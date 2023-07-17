faa4dcd0c6207dd79e42fd9c049ac4672138db90
Akena Mahesh Vardhan <113368513+Mahesh-Vardhan@users.noreply.github.com>
2023-07-17 07:50:32 +0530

    main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
-password_label = Label(text="password:")
+password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 #Entries
